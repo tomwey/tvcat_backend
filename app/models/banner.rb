d689f5720bd6443bdbfb73157638cba77be518e3
tomwey <tomwey@163.com>
2018-04-17 17:19:00 +0800

    add banner gen uniq_id

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,4 +2,15 @@ class Banner < ActiveRecord::Base
   validates :image, presence: true
   
   mount_uploader :image, BannerImageUploader
+  
+  before_create :generate_unique_id
+  def generate_unique_id
+    begin
+      n = rand(10)
+      if n == 0
+        n = 8
+      end
+      self.uniq_id = (n.to_s + SecureRandom.random_number.to_s[2..6]).to_i
+    end while self.class.exists?(:uniq_id => uniq_id)
+  end
 end
