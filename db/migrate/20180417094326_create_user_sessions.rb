fa99bfe2f93ed6f243daf7335a265d600627a4b8
tomwey <tomwey@163.com>
2018-04-17 17:48:14 +0800

    add some func

---
+++ db/migrate/20180417094326_create_user_sessions.rb
@@ -0,0 +1,26 @@
+class CreateUserSessions < ActiveRecord::Migration
+  def change
+    create_table :user_sessions do |t|
+      t.integer :uid
+      t.string :uniq_id
+      t.datetime :take_at
+      t.integer :take_type
+      t.st_point :location, geographic: true
+      t.string :app_version
+      t.string :ip
+      t.string :network
+      
+      t.string :uuid
+      t.string :os
+      t.string :os_version
+      t.string :model
+      t.string :lang_code
+      
+
+      t.timestamps null: false
+    end
+    add_index :user_sessions, :uid
+    add_index :user_sessions, :uniq_id, unique: true
+    add_index :user_sessions, :location, using: :gist
+  end
+end
