d929e71a8f06fe25ef98ccd059bf030c60dc7257
tomwey <tomwey@163.com>
2018-04-17 22:00:38 +0800

    ccc

--- app/admin/banner.rb
+++ app/admin/banner.rb
@@ -3,13 +3,20 @@ ActiveAdmin.register Banner do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 permit_params :image, :link, :opened, :sort
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+
+index do
+  selectable_column
+  column('ID',:uniq_id)
+  column :image, sortable: false do |o|
+    image_tag o.image.url(:small)
+  end
+  column :link, sortable: false
+  column :opened, sortable: false
+  column :sort
+  column 'at', :created_at
+  
+  actions
+end
 
 end
