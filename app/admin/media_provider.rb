ef0fe9e44357b68440b19b2564557c3b2b77bec6
tomwey <tomwey@163.com>
2018-04-17 21:53:21 +0800

    fixed bugs

--- app/admin/media_provider.rb
+++ app/admin/media_provider.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register MediaProvider do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :icon, :url, :sort, :opened
 #
 # or
 #
