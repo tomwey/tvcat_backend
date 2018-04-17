5849de32a465a1889c1c3ec44fe72439a55a90f1
tomwey <tomwey@163.com>
2018-04-17 22:04:30 +0800

    add some func

--- app/admin/media_provider.rb
+++ app/admin/media_provider.rb
@@ -3,13 +3,20 @@ ActiveAdmin.register MediaProvider do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 permit_params :name, :icon, :url, :sort, :opened
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+index do
+  selectable_column
+  column('ID',:uniq_id)
+  column :icon, sortable: false do |o|
+    image_tag o.icon.url(:large), size: '36x36'
+  end
+  column :name, sortable: false
+  column :url, sortable: false
+  column :opened, sortable: false
+  column :sort
+  column 'at', :created_at
+  
+  actions
+end
 
 end
