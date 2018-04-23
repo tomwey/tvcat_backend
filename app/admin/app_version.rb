b4c2e4b61801d0235cf6401392dbf779c6ad03a7
tomwey <tomwey@163.com>
2018-04-23 09:21:55 +0800

    change some func

--- app/admin/app_version.rb
+++ app/admin/app_version.rb
@@ -47,8 +47,8 @@ end
 form do |f|
   f.semantic_errors
   f.inputs '版本信息' do
-    f.input :version, label: '版本号', placeholder:'1.2.0'
     f.input :os, as: :select, label: '平台', collection: ['iOS', 'Android']
+    f.input :version, label: '版本号', placeholder:'1.2.0'
     # f.input :change_log, as: :text, label: '版本更新日志', rows: 10, cols: 30#, input_html: { class: 'redactor' }, placeholder: '网页内容，支持图文混排', hint: '网页内容，支持图文混排'
     # f.input :bundle_id, label: 'Bundle ID/包名'
     f.input :change_log, as: :text, input_html: { class: 'redactor' }, placeholder: '网页内容，支持图文混排', hint: '网页内容，支持图文混排'
