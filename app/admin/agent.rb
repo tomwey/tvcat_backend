30181bbb603ad810507a184ed6efdc04708e8602
tomwey <tomwey@163.com>
2018-06-17 10:26:46 +0800

    add some func

---
+++ app/admin/agent.rb
@@ -0,0 +1,47 @@
+ActiveAdmin.register Agent do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :mobile, :level, :login, :password, :password_confirmation, :parent_id, :verified
+
+index do
+  selectable_column
+  column('#',:id)
+  column 'ID', :uniq_id
+  column raw('<br>名字<br>手机<br>账号<br><br>') do |o|
+    raw("名字: #{o.name}<br>账号: #{o.login}<br>手机: #{o.mobile}")
+  end
+  column raw('<br>代理级别<br>佣金收入<br>账户余额<br><br>') do |o|
+    raw("代理级别: L#{o.level}<br>佣金收入: #{'%.2f' % (o.earn / 100.0) + '元'}<br>账户余额: #{'%.2f' % (o.balance / 100.0) + '元'}")
+  end
+  column '上级代理' do |o|
+    if o.parent_id.blank?
+      '--'
+    else
+      link_to "[#{o.parent.uniq_id}]#{o.parent.name}", [:admin, o.parent]
+    end
+  end
+  column :private_token
+  column :verified
+  column 'at', :created_at
+  actions
+end
+
+form do |f|
+  f.semantic_errors
+  f.inputs '基本信息' do
+    f.input :name
+    f.input :mobile
+    f.input :level, as: :select, collection: Agent.levels
+    f.input :parent_id, as: :select, label: '上级代理', collection: Agent.where(verified: true).map { |a| [a.name, a.uniq_id] }
+    f.input :verified
+  end
+  f.inputs "登录信息" do
+    f.input :login, placeholder: '需要保证唯一'
+    f.input :password, label: '密码'
+    f.input :password_confirmation, label: '确认密码'
+  end
+  actions
+end
+
+end
--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,38 @@
 class Agent < ActiveRecord::Base
+  has_secure_password
+  
+  validates :name, :mobile, :login, :level, presence: true
+  
+  validates :password,
+      presence: { on: :create },
+      length: { minimum: 6, allow_blank: true }
+      
+  validate :require_parent
+  def require_parent
+    if self.level > 0
+      errors.add(:parent_id, '必须指定上级代理')
+      return false
+    end
+  end
+  
+  before_create :generate_uniq_id_and_private_token
+  def generate_uniq_id_and_private_token
+    begin
+      n = rand(10)
+      if n == 0
+        n = 8
+      end
+      self.uniq_id = (n.to_s + SecureRandom.random_number.to_s[2..8]).to_i
+    end while self.class.exists?(:uniq_id => uniq_id)
+    self.private_token = SecureRandom.uuid.gsub('-', '')
+  end
+  
+  def self.levels
+    [['L0', 0], ['L1', 1], ['L2', 2]]
+  end
+  
+  def parent
+    @parent ||= Agent.find_by(uniq_id: self.parent_id)
+  end
+  
 end
