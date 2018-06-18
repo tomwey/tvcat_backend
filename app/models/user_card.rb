4f006bb3e3c3fbf5fa84843b506f82d4207c7d8d
tomwey <tomwey@163.com>
2018-06-18 21:05:58 +0800

    ccc

--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -1,4 +1,16 @@
 class UserCard < ActiveRecord::Base
+  
+  before_create :generate_unique_id
+  def generate_unique_id
+    begin
+      n = rand(10)
+      if n == 0
+        n = 8
+      end
+      self.uniq_id = (n.to_s + SecureRandom.random_number.to_s[2..8]).to_i
+    end while self.class.exists?(:uniq_id => uniq_id)
+  end
+  
   def card_ad
     @card_ad ||= CardAd.find_by(uniq_id: self.card_ad_id)
   end
