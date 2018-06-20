1b0de0ed7c42bbdc59cd728a197dfe4358bb9e99
tomwey <tomwey@163.com>
2018-06-20 14:30:55 +0800

    ccc

--- db/migrate/20180418020831_change_columns_for_user_sessions.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsForUserSessions < ActiveRecord::Migration
-  def change
-    rename_column :user_sessions, :take_at, :begin_time
-    add_column :user_sessions, :end_time, :datetime
-  end
-end
