e58a5366c37bff24c8c98f5ca9b836b6f70cd556
tomwey <tomwey@163.com>
2018-08-19 00:14:45 +0800

    ccc

--- lib/api/v1/users_api.rb
+++ lib/api/v1/users_api.rb
@@ -135,7 +135,7 @@ module API
                 download_url: SiteConfig.app_qrcode_url,
                 vip_charge_url: SiteConfig.vip_charge_url,
                 faq_url: SiteConfig.faq_url,
-                ad_blacklist: SiteConfig.ad_blacklist.split(','),
+                ad_blacklist: [],
                 ad_script: "var $el = $('a[id^=__a_z_]'); $el.hide();"
               }
             }
